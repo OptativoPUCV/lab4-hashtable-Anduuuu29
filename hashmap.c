fe23729562590e38a73fdc63ce71f30fd5711632
Anduuuu29 <163463400+Anduuuu29@users.noreply.github.com>
2024-04-25 03:09:51 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -57,7 +57,6 @@ void enlarge(HashMap * map) {
 HashMap * createMap(long capacity) {
   
   HashMap * map = (HashMap *) malloc (sizeof(HashMap));
-  map->buckets = (Pair **) calloc (capacity, sizeof(Pair *));
   map->size = 0;
   map->capacity = capacity;
   map->current = -1;
